9d068cf64d9805d7b05817a55c843278e4caebfb
Daniel Terranova <daniel.terranova@gmail.com>
2020-10-03 21:02:11 +0200

    work

--- src/test/java/com/ngm/exercise/ordermatcher/OrderMatcherTest1.java
+++ src/test/java/com/ngm/exercise/ordermatcher/OrderMatcherTest1.java
@@ -15,14 +15,14 @@ public class OrderMatcherTest1 {
 
     private static Stream<Arguments> provider() {
         return Stream.of(
-            Arguments.of(of("BUY 100@10", "SELL 60@10"), of("TRADE 60@10")),
-            Arguments.of(of("BUY 50@9", "BUY 100@10", "SELL 150@10"), of("TRADE 100@10")),
-            Arguments.of(of("SELL 50@11", "SELL 100@10", "BUY 150@10"), of("TRADE 100@10")),
-            Arguments.of(of("SELL 50@10", "SELL 100@10", "BUY 150@10"), of("TRADE 50@10", "TRADE 100@10")),
-            Arguments.of(of("SELL 100@10", "BUY 100@11"), of("TRADE 100@10")),
-            Arguments.of(of("BUY 100@10", "SELL 100@9"), of("TRADE 100@10")),
+            Arguments.of(of("BUY 100@7", "SELL 60@7"), of("TRADE 60@7")),
+            Arguments.of(of("BUY 50@19", "BUY 100@20", "SELL 150@20"), of("TRADE 100@20")),
+            Arguments.of(of("SELL 50@22", "SELL 100@10", "BUY 150@10"), of("TRADE 100@10")),
+            Arguments.of(of("SELL 50@20", "SELL 100@20", "BUY 150@20"), of("TRADE 50@20", "TRADE 100@20")),
+            Arguments.of(of("SELL 100@9", "BUY 100@11"), of("TRADE 100@9")),
+            Arguments.of(of("BUY 100@9", "SELL 100@8"), of("TRADE 100@9")),
             Arguments.of(of("BUY 100@10", "BUY 100@10", "BUY 100@11", "BUY 100@9", "SELL 100@100", "SELL 100@100", "SELL 100@101", "SELL 100@99"), of()),
-            Arguments.of(of("BUY 100@10", "BUY 100@10", "BUY 100@11", "BUY 100@9"), of()));
+            Arguments.of(of("BUY 100@15", "BUY 100@15", "BUY 100@16", "BUY 100@14"), of()));
 
     }
 
