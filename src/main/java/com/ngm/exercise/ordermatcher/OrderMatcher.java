31444436390a3897a53a821656942d551dbf362c
Daniel Terranova <daniel.terranova@gmail.com>
2020-10-03 21:12:10 +0200

    work

--- src/main/java/com/ngm/exercise/ordermatcher/OrderMatcher.java
+++ src/main/java/com/ngm/exercise/ordermatcher/OrderMatcher.java
@@ -17,28 +17,19 @@ public class OrderMatcher {
 
     public static void main(final String... args) {
         OrderMatcher matcher = new OrderMatcher();
-        System.out.println("Order matcher. Type 'help' for a list of commands. To quit hit 'Ctrl+d' or 'QUIT'");
+        System.out.println("Order matcher. To quit hit 'Ctrl+d' or 'QUIT'");
         System.out.println();
         Scanner scanner = new Scanner(System.in);
         String line;
-        LOOP:
         while (scanner.hasNext()) {
             line = scanner.nextLine();
             line = line.trim();
             try {
-                switch (line) {
-                    case "HELP":
-                        System.out.println("Available commands: \n"
-                            + "  BUY|SELL <quantity>@<price>  - Enter an order.\n"
-                            + "  PRINT                         - List all remaining orders.\n"
-                            + "  QUIT                         - Quit.\n"
-                            + "  HELP                         - Show help (this message).\n");
-                        break;
+                switch (line.toUpperCase()) {
                     case "":
-                        // ignore
                         break;
                     case "QUIT":
-                        break LOOP;
+                        return;
                     case "PRINT":
                         System.out.println("--- BUY ---");
                         print(matcher.getOrders(Side.BUY));
@@ -52,12 +43,12 @@ public class OrderMatcher {
                         break;
                 }
             } catch (final IllegalArgumentException e) {
-                System.err.println("Bad input: " + e.getMessage());
-            } catch (final UnsupportedOperationException e) {
-                System.err.println("Sorry, this command is not supported yet: " + e.getMessage());
+                System.err.println(e.getMessage());
+            }
+            catch (final Exception e) {
+                System.err.println("Error: " + e.getMessage());
             }
         }
-        System.out.println("Good bye!");
     }
 
     private static void print(final List<?> orders) {
